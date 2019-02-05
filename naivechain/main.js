d9350da7afc19a174ff166a03285993ab657c2a8
João Silva Borralho <NB24546@novabase.pt>
2019-02-05 16:20:19 +0000

    args bug

--- naivechain/main.js
+++ naivechain/main.js
@@ -1,8 +1,12 @@
 var server = require("./server.js");
 var Blockchain = require("./blockchain.js");
-var http_port = process.env.HTTP_PORT || 3001;
-var p2p_port = process.env.P2P_PORT || 6001;
-var initialPeers = process.env.PEERS ? process.env.PEERS.split(",") : [];
+
+//var http_port = process.env.HTTP_PORT || 3001;
+//var p2p_port = process.env.P2P_PORT || 6001;
+var http_port = process.argv[2];
+var p2p_port = process.argv[3];
+var initialPeers = process.argv[4] ? process.argv[4].split(",") : [];
+//var initialPeers = process.env.PEERS ? process.env.PEERS.split(",") : [];
 console.log(server);
 server.connectToPeers(initialPeers);
 server.initHttpServer(http_port);
